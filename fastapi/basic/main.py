d63340bf2fc481f6daceac2a4232eb3fb7996bcd
Mohamed Yosef <mohamed101yosef@outlook.com>
2026-02-02 13:22:48 +0200

    BaseModel: Item

--- fastapi/basic/main.py
+++ fastapi/basic/main.py
@@ -1,13 +1,23 @@
 from typing import Union as union 
 
 from fastapi import FastAPI
+from pydantic import BaseModel 
 
 app = FastAPI()
+
+class Item(BaseModel): 
+    name: str 
+    price: float 
+    is_available: bool 
 
 @app.get("/")
 def read_root(): 
     return {"tool": "fastapi"}
 
 @app.get("/item/{item_id}")
-def read_item(item_id: int, item_info: union[str, None] = None): 
-    return {"item id": item_id, "info": item_info}
+def read_item(item_id: int, query: union[str, None] = None): 
+    return {"item id": item_id, "Query": query}
+
+@app.put("/items/{item_id}")
+def update_item(item_id, item: Item): 
+    return {"item name": item.name, "item id": item_id} 
